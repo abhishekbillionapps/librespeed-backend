2a61bb0141de3bd39035303d686dfdacd0970e1d
Abhishek Bisht <abhishek@billionapps.net>
2025-07-04 19:18:39 +0530

    7th commit

--- index.js
+++ index.js
@@ -6,16 +6,34 @@ const app = express();
 app.use(cors()); // Allow all origins
 app.use(express.raw({ type: '*/*', limit: '200mb' })); // Handle big uploads
 
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  console.log('Body:', req.body);
+  console.log('Query:', req.query);
+  console.log('Headers:', req.headers);
+  next();
+});
+
+
 // Dummy upload handler
 app.post('/empty', (req, res) => {
   req.on('data', () => { }); // Do nothing with the data
   req.on('end', () => res.status(200).send('OK'));
 });
 
+
 app.post('/check-route', (req, res) => {
   return res.status(200).json({
     status: 'success',
     message: 'Hellooooo thereA PP World!',
+  });
+});
+
+app.post('/', (req, res) => {
+  return res.status(200).json({
+    status: 'success',
+    message: 'Hellooooo there World! from India',
   });
 });
 
